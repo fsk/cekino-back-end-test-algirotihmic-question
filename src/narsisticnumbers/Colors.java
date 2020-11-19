d04e651e6a451aa5ed740f2ca6bcbb5cb01dd9b8
frknshnklksz <furkankulaksiz68@gmail.com>
2020-11-19 23:05:25 +0300

    BugFix -> Narsistik sayı olma özelliği taşıyan tek basamaklı sayılar da koda eklendi.
    Geçersiz durumlar için gerekli exceptionlar yazıldı..

---
+++ src/narsisticnumbers/Colors.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package narsisticnumbers;
+
+/**
+ *
+ * @author USER
+ */
+public class Colors {
+    
+    private Colors(){
+        
+    }
+
+    public static final String Red = "\u001b[31m";
+    public static final String Green = "\u001b[32m";
+    public static final String Yellow = "\u001b[33m";
+    public static final String Blue = "\u001b[34m";
+    public static final String Magenta = "\u001b[35m";
+    public static final String Cyan = "\u001b[36m";
+    //public static final String bg = "\u001b[40;1m";
+
+}
--- src/narsisticnumbers/NarsisticNumbers.java
+++ src/narsisticnumbers/NarsisticNumbers.java
@@ -5,27 +5,56 @@ import java.util.Scanner;
 
 public class NarsisticNumbers {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         boolean result = isNarsisticNumber(enteredNumber());
-        
-        if(result == true){
-            System.out.println("This number is a narsistic number.");
-        }else{
-            System.out.println("This number is not a narsistic number.");
+
+        if (result == true) {
+            System.out.println(Colors.Red + "This number is a narsistic number.");
+        } else {
+            System.out.println(Colors.Red + "This number is not a narsistic number.");
         }
 
     }
 
     public static String enteredNumber() {
         Scanner data = new Scanner(System.in);
-        System.out.print("Please enter a number: ");
+        System.out.print(Colors.Green + "Please enter a number: ");
         String numberString = data.next();
         String newNumberString = numberString.trim();
         return newNumberString;
     }
 
-    public static boolean isNarsisticNumber(String number) {
+    public static boolean isNarsisticNumber(String number) throws Exception {
+
+        if (number.contains(",") || number.contains(".")) {
+            throw new Exception(Colors.Blue + "This is a invalid number. Float or Double numbers can not Narsistic Number.");
+        }
+        
+        try{
+            int i = Integer.parseInt(number.trim());
+        }catch(IllegalArgumentException nfe){
+            throw new NumberFormatException(Colors.Yellow + "This is not a number or This number is overbig.");
+        }
+        
+        if(number.length() > 10){
+            throw new Exception(Colors.Yellow + "This number is a overbig.");
+        }
+
+        int convertNumberForFirstControl = Integer.parseInt(number.trim());
+        
+        if (convertNumberForFirstControl < 0) {
+            throw new Exception(Colors.Cyan + "This is a invalid number. Negative numbers can not Narsistic Numbers. ");
+        }
+        
+        if(convertNumberForFirstControl == 0){
+            throw new Exception(Colors.Cyan + "0 is not a Narsistic Number. Because 0 pow 0 = infinite");
+        }
+
+        
+        if (convertNumberForFirstControl >= 1 && convertNumberForFirstControl <= 9) {
+            return true;
+        }
 
         ArrayList<Character> numberTextList = new ArrayList<>();
         for (Character c : number.toCharArray()) {
@@ -54,7 +83,6 @@ public class NarsisticNumbers {
         }
 
     }
-  
 
     public static int getPow(int a, int b) {
         if (a == 0 && b == 0) {
